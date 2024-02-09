f142d000fa8e51ea558d6f277b73c9ad199ed020
michael-m-2983 <116770284+michael-m-2983@users.noreply.github.com>
2024-02-08 19:36:24 -0600

    Add forward kinematics code

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -3,16 +3,15 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.ControlModeValue;
 
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
-import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
-import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.Constants.ArmConstants;
+
+import static java.lang.Math.*;
 
 public class ArmSubsystem extends SubsystemBase {
     // -------- First Joint ------- \\
@@ -34,26 +33,12 @@ public class ArmSubsystem extends SubsystemBase {
     private final ProfiledPIDController wristFeedback = new ProfiledPIDController(ArmConstants.WRIST_kP, ArmConstants.WRIST_kI, ArmConstants.WRIST_kD, ArmConstants.WRIST_CONSTRAINTS);
 
 
-    // -------- Smartdashboard ----- \\
-    Mechanism2d mech = new Mechanism2d(2, 2);
-    MechanismRoot2d arm = mech.getRoot("arm", 0, 0);
-    MechanismLigament2d shoulder = arm.append(new MechanismLigament2d("shoulder", 3, 90));
-    MechanismLigament2d wrist = shoulder.append(new MechanismLigament2d("wrist", 1, 0));
-
-
     public ArmSubsystem () {
         left_firstJointMotor.setInverted(ArmConstants.LEFT_FIRST_MOTOR_REVERSED);
         right_firstJointMotor.setInverted(ArmConstants.RIGHT_FIRST_MOTOR_REVERSED);
         right_firstJointMotor.setControl(new Follower(left_firstJointMotor.getDeviceID(), true));
 
         secondJointMotor.setInverted(ArmConstants.SECOND_MOTOR_REVERSED);
-    }
-
-    @Override
-    public void periodic() {
-        shoulder.setAngle(firstJointEncoder.getPosition().getValue());
-        wrist.setAngle(secondJointEncoder.getPosition().getValue());
-        SmartDashboard.putData("Arm", mech);   
     }
 
     public void setWrist(double setpoint, double velocity) {
@@ -68,5 +53,18 @@ public class ArmSubsystem extends SubsystemBase {
         double feedforward = shoulderFeedforward.calculate(setpoint, velocity);
 
         left_firstJointMotor.setVoltage(feedback + feedforward);
+    }
+
+    public Translation2d getCurrentEndpoint() {
+        final double length1 = Constants.ArmConstants.L1;
+        final double length2 = Constants.ArmConstants.L2;
+
+        final double angle1 = firstJointEncoder.getPosition().getValueAsDouble();
+        final double angle2 = secondJointEncoder.getPosition().getValueAsDouble();
+
+        final Translation2d a = new Translation2d(sin(angle1), -cos(angle1)).times(length1);
+        final Translation2d b = new Translation2d(sin(angle1 + angle2), -cos(angle1 + angle2)).times(length2);
+
+        return a.plus(b);
     }
 }
