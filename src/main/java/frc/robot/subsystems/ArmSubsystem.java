42699c32afac1fa44dee87799468c4227c9e3e19
Spoopr <qnguyenhammel@gmail.com>
2024-02-08 19:32:32 -0600

    shuffleboard representation of arm

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -7,6 +7,10 @@ import com.ctre.phoenix6.signals.ControlModeValue;
 
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ArmConstants;
 
@@ -30,12 +34,26 @@ public class ArmSubsystem extends SubsystemBase {
     private final ProfiledPIDController wristFeedback = new ProfiledPIDController(ArmConstants.WRIST_kP, ArmConstants.WRIST_kI, ArmConstants.WRIST_kD, ArmConstants.WRIST_CONSTRAINTS);
 
 
+    // -------- Smartdashboard ----- \\
+    Mechanism2d mech = new Mechanism2d(2, 2);
+    MechanismRoot2d arm = mech.getRoot("arm", 0, 0);
+    MechanismLigament2d shoulder = arm.append(new MechanismLigament2d("shoulder", 3, 90));
+    MechanismLigament2d wrist = shoulder.append(new MechanismLigament2d("wrist", 1, 0));
+
+
     public ArmSubsystem () {
         left_firstJointMotor.setInverted(ArmConstants.LEFT_FIRST_MOTOR_REVERSED);
         right_firstJointMotor.setInverted(ArmConstants.RIGHT_FIRST_MOTOR_REVERSED);
         right_firstJointMotor.setControl(new Follower(left_firstJointMotor.getDeviceID(), true));
 
         secondJointMotor.setInverted(ArmConstants.SECOND_MOTOR_REVERSED);
+    }
+
+    @Override
+    public void periodic() {
+        shoulder.setAngle(firstJointEncoder.getPosition().getValue());
+        wrist.setAngle(secondJointEncoder.getPosition().getValue());
+        SmartDashboard.putData("Arm", mech);   
     }
 
     public void setWrist(double setpoint, double velocity) {
