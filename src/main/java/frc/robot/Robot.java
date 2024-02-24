65c3edfe8eeeb769aa4110bb7c2f06ab3117fa9e
voidReq <jhanstedtas@gmail.com>
2024-02-24 13:46:20 -0600

    R, G, B on some LEDs

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.subsystems.LEDstripOne;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -21,6 +22,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * project.
  */
 public class Robot extends TimedRobot {
+  LEDstripOne m_stripOne = new LEDstripOne(9);
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
---
+++ src/main/java/frc/robot/subsystems/Buffer2.java
@@ -0,0 +1,21 @@
+/* 
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+
+public class Buffer2 extends AddressableLEDBuffer {
+
+    public Buffer2(int length) {
+        // super(length);
+        // this.m_
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void setRGB(int index, int r, int g, int b) {
+        // TODO Auto-generated method stub
+        super.setRGB(index, r, g, b);
+    }
+    
+}
+ */
