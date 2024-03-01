c16207ed87412003ce48e561da5306b84fe8b268
voidReq <jhanstedtas@gmail.com>
2024-02-29 19:03:45 -0600

    MMM I like bacon

--- src/main/java/frc/robot/subsystems/LEDstripOne.java
+++ src/main/java/frc/robot/subsystems/LEDstripOne.java
@@ -107,10 +107,11 @@ public class LEDstripOne extends SubsystemBase {
         double ltmult = ctime / 20.0;
 
         double mtime = DriverStation.getMatchTime();
+
         if(DriverStation.isAutonomousEnabled()){
             rainbow();
         }
-        if (DriverStation.isTeleopEnabled() && mtime <= ctime) {
+        else if (DriverStation.isTeleopEnabled() && mtime <= ctime) {
             for (int i = 0; i < 20; i++) {
                 // Countdown
                 int r = MathUtil.clamp((int) (255 * ((1.5 * ctime - 2 * mtime) / ctime)), 0, 255);
